4f3bf3d63f1cca4a6a3be339b30b9f1ac6dbd6b9
sclarkstone <samclarkstone@hotmail.com>
2021-11-10 09:23:20 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,14 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
 
 
